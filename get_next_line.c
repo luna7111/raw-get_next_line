519f8b5d7744b10a4215fd8eecc108dd42061bd9
Luna Del Valle Blazquez <ldel-val@c1r11s3.42madrid.com>
2024-10-14 20:34:02 +0200

    unfinished changes, started a function that trims all characters before a breakline.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 19:29:06 by ldel-val          #+#    #+#             */
-/*   Updated: 2024/10/14 19:43:13 by ldel-val         ###   ########.fr       */
+/*   Updated: 2024/10/14 20:33:39 by ldel-val         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,27 @@ long	ft_find_linebreak(char *string)
 	}
 	return (-1);
 }
+
+void	ft_trim_before_break(char *string)
+{
+	size_t i;
+
+	if (!ft_find_linebreak)
+		return ;
+	i = 0;
+}
+
+char *get_next_line(fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*string
+	size_t		current_position
+
+	current_position = 0;
+	if (ft_find_linebreak(buffer) < 0)
+		ft_trim_before_break(buffer);
+}
+
 /*
 int	main(void)
 {
